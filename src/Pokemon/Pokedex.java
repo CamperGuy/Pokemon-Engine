f0235b27b0722ed7b655d8169b0269b8dd87c765
Joel Helbling <helblingjoel@gmail.com>
2019-02-02 20:35:45 +0000

    Initial Commit

---
+++ src/Pokemon/Pokedex.java
@@ -0,0 +1,10 @@
+package Pokemon;
+import java.util.HashMap;
+public class Pokedex {
+    private HashMap<Integer, PKMN_Descriptive> database;
+
+    private HashMap<Integer, Boolean> seenDB;
+    private HashMap<Integer, Boolean> caughtDB;
+
+
+}
